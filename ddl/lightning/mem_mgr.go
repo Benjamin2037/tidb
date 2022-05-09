74c86151972d9b4b35e7a9a6b724102d0942613a
Benjamin2037 <bear.c@pingcap.com>
2022-05-09 14:02:31 +0800

    uniform lightning related errmessage text to lightning_error file.

--- ddl/lightning/mem_mgr.go
+++ ddl/lightning/mem_mgr.go
@@ -18,15 +18,6 @@ const (
 	BUILD_BACKEND_CONTEXT BuildType = 1
 	BUILD_ENGINE_INFO     BuildType = 2
 	BUILD_WORKER_CONTEXT  BuildType = 3
-
-	// Error msg
-	LERR_ALLOC_MEM_FAILED      string = "Lightning: Alloc memory failed."
-	LERR_OUT_OF_MAX_MEM        string = "Lightning: Memory is used up for Lightning add index."
-	LERR_NO_MEM_TYPE           string = "Lightning: Unking structure for Lightning add index."
-	LERR_CREATE_BACKEND_FAILED string = "Lightning: Build lightning backend failed, will use kernel index reorg method to backfill the index."
-	LERR_CREATE_ENGINE_FAILED  string = "Lightning: Build lightning engine failed, will use kernel index reorg method to backfill the index."
-	LERR_CREATE_CONTEX_FAILED  string = "Lightning: Build lightning worker context failed, will use kernel index reorg method to backfill the index."
-	LERR_GET_ENGINE_FAILED     string = "Lightning: Use key get engininfo failed."
 )
 
 // MemoryRoot is used to trace the memory usage of all light DDL environment.
